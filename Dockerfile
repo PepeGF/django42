f893a2801569b54427554c76ae1b8bfe15b19d09
PepeGF <josgarci@student.42madrid.com>
2026-04-04 16:05:15 +0200

    Translations done

--- Dockerfile
+++ Dockerfile
@@ -11,12 +11,16 @@ RUN apt-get update && apt-get install -y \
     net-tools \
     iputils-ping \
     unzip \
+    gettext \
     zip \
     build-essential \
     && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-RUN pip install --no-cache-dir Django requests psycopg2-binary \
-                            gunicorn dotenv django-bootstrap4
+RUN pip install --no-cache-dir Django==5.2.12 \
+                               psycopg2-binary==2.9.11 \
+                               django-bootstrap5==26.2
+# requests \
+# gunicorn dotenv \
 
 EXPOSE 8000
 
@@ -28,3 +32,4 @@ CMD ["/bin/bash"]
 # docker run -p 8000:8000 -it --rm -v "$(pwd)":/workspace navaja-suiza 
 # python manage.py runserver 0.0.0.0:8000
 # alias rt="python3 manage.py runserver 0.0.0.0:8000"
+# apt-get update && apt-get install -y gettext
---
+++ wololo.py
@@ -0,0 +1,121 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Article, UserFavouriteArticle
+
+User = get_user_model()
+
+
+class AccessControlAndFavouritesTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="u", password="p")
+        self.other = User.objects.create_user(username="o", password="p")
+        self.article = Article.objects.create(
+            title="T", author=self.other, synopsis="synopsis", content="content"
+        )
+
+    def test_favourites_view_requires_authentication_and_uses_template(self):
+        url = reverse('ex:favourites')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/favourite.html")
+
+    def test_publications_view_requires_authentication_and_uses_template(self):
+        url = reverse('ex:publications')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/publications.html")
+
+    def test_publish_view_requires_authentication_and_uses_template(self):
+        url = reverse('ex:publish')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/publish.html")
+
+    def test_authenticated_user_cannot_access_registration_form(self):
+        url = reverse('ex:register')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertNotEqual(resp.status_code, 200)
+
+    def test_user_cannot_add_same_article_twice_to_favourites(self):
+        add_url = reverse('ex:add-favourite', kwargs={'pk': self.article.pk})
+        self.client.login(username="u", password="p")
+        resp1 = self.client.post(add_url, follow=True)
+        self.assertEqual(
+            UserFavouriteArticle.objects.filter(user=self.user, article=self.article).count(), 1
+        )
+        resp2 = self.client.post(add_url, follow=True)
+        self.assertEqual(
+            UserFavouriteArticle.objects.filter(user=self.user, article=self.article).count(), 1
+        )
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Article, UserFavouriteArticle
+
+User = get_user_model()
+
+
+class FavouritesAndAccessTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="u", password="p")
+        self.other = User.objects.create_user(username="o", password="p")
+        self.article = Article.objects.create(
+            title="T", author=self.other, synopsis="synopsis", content="content"
+        )
+
+    def test_favourites_view_requires_login_and_uses_template_for_authenticated(self):
+        url = reverse('ex:favourites')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/favourite.html")
+
+    def test_publications_view_requires_login_and_uses_template_for_authenticated(self):
+        url = reverse('ex:publications')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/publications.html")
+
+    def test_publish_view_requires_login_and_uses_template_for_authenticated(self):
+        url = reverse('ex:publish')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ex/publish.html")
+
+    def test_authenticated_user_cannot_access_registration_form(self):
+        url = reverse('ex:register')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.client.login(username="u", password="p")
+        resp = self.client.get(url)
+        self.assertNotEqual(resp.status_code, 200)
+
+    def test_user_cannot_add_same_article_twice_to_favourites(self):
+        add_url = reverse('ex:add-favourite', kwargs={'pk': self.article.pk})
+        self.client.login(username="u", password="p")
+        resp1 = self.client.post(add_url, follow=True)
+        self.assertEqual(UserFavouriteArticle.objects.filter(user=self.user, article=self.article).count(), 1)
+        resp2 = self.client.post(add_url, follow=True)
+        self.assertEqual(UserFavouriteArticle.objects.filter(user=self.user, article=self.article).count(), 1)
+from django.test import TestCase
